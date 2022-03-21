2e76c7ca1c1a396fb70f6628dcb33d8d9e9e4f92
AntonAntonaa <7918547235@yandex.ru>
2022-03-21 21:31:20 +0300

    add jwt, crypto, validator

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const appUtils = require("./utils");
 const db = require("./models");
-const { request } = require("express");
-const { response } = require("express");
 const jwt = require("jsonwebtoken");
-const token = jwt.sign({ foo: "bar" }, "shhhhh");
-const bcrypt = require(" bcryptjs ");
+const bcrypt = require("bcryptjs");
+const { body, param, validationResult } = require("express-validator");
 
 const jsonParser = express.json();
 const app = express();
-
 
 app.use(jsonParser);
 app.use(express.urlencoded());
@@ -18,76 +15,112 @@ app.get("/health", appUtils.authenticateToken, (req, res) => {
   return res.status(200).json({ OK: "da" });
 });
 
-app.post("/users", async (request, response) => {
-  if (!request.body) return response.sendStatus(400);
+app.post(
+  "/users",
+  appUtils.authenticateToken,
+  body("userName").isLength({ min: 5 }),
+  body("email").isEmail(),
+  body("dob").isDate(),
+  body("password").isLength({ min: 8 }),
+  async (request, response) => {
+    if (!request.body) return response.sendStatus(400);
 
-  try {
-    const userPayload = appUtils.validateUserData(request.body);
-    await db.user.create(userPayload);
-    return response.send(userPayload);
-  } catch (e) {
-    console.log(e.massage);
-    return response.status(400).json({ message: e.message });
+    try {
+      const userPayload = appUtils.validateUserData(request.body);
+      await db.user.create(userPayload);
+      return response.send(userPayload);
+    } catch (e) {
+      console.log(e.massage);
+      return response.status(400).json({ message: e.message });
+    }
   }
-});
+);
 
 app.get("/users", async (request, response) => {
   const allUsers = await db.user.findAll();
   return response.send(allUsers);
 });
 
-app.get("/users/:id", appUtils.authenticateToken, async (request, response) => {
-  const userid = request.params.id;
-  try {
-    const user = await appUtils.getUserById(userid);
-    return response.send(user);
-  } catch (e) {
-    return response.status(404).json({ message: e.message });
-  }
-});
-
-app.put("/users/:id", async (request, response) => {
-  const userid = request.params.id;
-  try {
-    await appUtils.getUserById(userid);
-  } catch (e) {
-    return response.status(404).json({ message: e.message });
-  }
-  try {
-    const userPayload = appUtils.validateUserData(request.body);
-    const user = await appUtils.updateUser(userid, userPayload);
-    return response.send(user);
-  } catch (e) {
-    return response.status(400).json({ message: e.message });
-  }
-});
-
-app.delete("/users/:id", async (request, response) => {
-  const userid = request.params.id;
-  try {
-    await appUtils.getUserById(userid);
-  } catch (e) {
-    return response.status(404).json({ message: e.message });
-  }
-  try {
-    await appUtils.deleteUser(userid);
-    return response.sendStatus(200);
-  } catch (e) {
-    return response.status(500).json({ message: e.message });
-  }
-});
-
-app.post("/auth/login", async (req, res) => {
-  try {
-    const dbUser = db.users.findOne({ where: req.body.userName });
-    if (!dbUser || dbUser.password !== req.body.password) {
-      throw new Error("WRONG");
+app.get(
+  "/users/:id",
+  appUtils.authenticateToken,
+  param("id").isNumeric({ no_symbols: true }),
+  async (request, response) => {
+    const userid = request.params.id;
+    try {
+      const user = await appUtils.getUserById(userid);
+      return response.send(user);
+    } catch (e) {
+      return response.status(404).json({ message: e.message });
     }
-    return res.status(404).json({ message: "User not found" });
-  } catch (error) {
-    return res.status(500).json({ message: error.massage });
   }
-});
+);
+
+app.put(
+  "/users/:id",
+  appUtils.authenticateToken,
+  param("id").isNumeric({ no_symbols: true }),
+  body("userName").isLength({ min: 5 }),
+  body("email").isEmail(),
+  body("dob").isDate(),
+  body("password").isLength({ min: 8 }),
+  async (request, response) => {
+    const userid = request.params.id;
+    try {
+      await appUtils.getUserById(userid);
+    } catch (e) {
+      return response.status(404).json({ message: e.message });
+    }
+    try {
+      const userPayload = appUtils.validateUserData(request.body);
+      const user = await appUtils.updateUser(userid, userPayload);
+      return response.send(user);
+    } catch (e) {
+      return response.status(400).json({ message: e.message });
+    }
+  }
+);
+
+app.delete(
+  "/users/:id",
+  appUtils.authenticateToken,
+  param("id").isNumeric({ no_symbols: true }),
+  async (request, response) => {
+    const userid = request.params.id;
+    try {
+      await appUtils.getUserById(userid);
+    } catch (e) {
+      return response.status(404).json({ message: e.message });
+    }
+    try {
+      await appUtils.deleteUser(userid);
+      return response.sendStatus(200);
+    } catch (e) {
+      return response.status(500).json({ message: e.message });
+    }
+  }
+);
+
+app.post(
+  "/auth/login",
+  body("userName").isLength({ min: 5 }),
+  body("password").isLength({ min: 8 }),
+  async (req, res) => {
+    try {
+      const dbUser = db.users.findOne({ where: req.body.userName });
+      if (
+        !dbUser ||
+        !appUtils.validatePassword(req.body.password, dbUser.password)
+      ) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+      delete user.password;
+      return res.json({ token: jwt.sign(user, tokenKey) });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+);
 
 app.listen(3002, () => {
   console.log("Application listening on port 3002!");
--- utils.js
+++ utils.js
@@ -1,18 +1,18 @@
 const moment = require("moment");
 const db = require("./models");
+const bcrypt = require("bcryptjs");
 
 function validateUserData(body) {
   if (!body.userName || !body.email || !body.password || !body.dob) {
     throw new Error("Please pass correct user data");
   }
   const salt = bcrypt.genSaltSync(10);
-  const passwordUser = password;
-  const passwordToSave = bcrypt.hashSync(passwordUser, salt);
+  const passwordToSave = bcrypt.hashSync(body.password, salt);
   const dob = moment(body.dob, "DD-MM-YYYY");
   const user = {
     userName: body.userName,
     email: body.email,
-    password: body.password,
+    password: passwordToSave,
     dob: dob.toDate(),
   };
   return user;
@@ -41,26 +41,22 @@ async function deleteUser(id) {
 }
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers["Authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) {
     return res.status(401).json({ message: "Authorization token is missing" });
   }
-
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     console.log(err);
-    if (err) return res.sendStatus(403);
+    if (err) return res.sendStatus(401);
     req.user = user;
     next();
   });
 }
-function passwordCheck (err, rows) {
-  if (err) {
-      return done(err);
-  }
-  if (bcrypt.hashSync(passwordEnteredByUser, salt) === rows[0].password) {
-    
-  }
+
+function validatePassword(password, hashToCompare) {
+  return bcrypt.compareSync(password, hashToCompare);
+}
 
 module.exports = {
   validateUserData,
@@ -68,5 +64,5 @@ module.exports = {
   getUserById,
   deleteUser,
   authenticateToken,
-  passwordCheck
+  validatePassword,
 };
