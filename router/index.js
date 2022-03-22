bcdc261067fbce1b7c9980ceef6db36fb1365ea9
Anton <89185747235@yndex.ru>
2022-03-22 15:35:39 +0300

    \

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const usersRoutes = require('./users.routes')
+//const authRoutes = require('./auth.routes')
 
 router.use('/users', usersRoutes)
 //router.use('/auth', authRoutes)
